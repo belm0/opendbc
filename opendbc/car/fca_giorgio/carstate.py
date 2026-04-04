46aedad20829436e2b4d398100f1ab778a1565a3
John Belmonte <john@neggie.net>
2026-04-03 20:51:51 -0700

    ENGINE_1 / ACCEL_PEDAL confirmed good

--- opendbc/car/fca_giorgio/carstate.py
+++ opendbc/car/fca_giorgio/carstate.py
@@ -34,9 +34,8 @@ class CarState(CarStateBase):
     ret.yawRate = pt_cp.vl["ABS_2"]["YAW_RATE"]
     ret.steerFaultPermanent = bool(pt_cp.vl["EPS_2"]["LKA_FAULT"])
 
-    # TODO: unsure if this is accel pedal or engine throttle
-    # ret.gas = pt_cp.vl["ENGINE_1"]["ACCEL_PEDAL"]
-    # ret.gasPressed = ret.gas > 0
+    ret.gas = pt_cp.vl["ENGINE_1"]["ACCEL_PEDAL"]
+    ret.gasPressed = ret.gas > 0
     ret.brake = pt_cp.vl["ABS_4"]["BRAKE_PRESSURE"]
     ret.brakePressed = bool(pt_cp.vl["ABS_3"]["BRAKE_PEDAL_SWITCH"])
     #ret.parkingBrake = TODO
