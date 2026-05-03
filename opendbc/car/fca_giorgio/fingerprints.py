5c641313e8a02c17d9d61b15691855f019a39004
John Belmonte <john@neggie.net>
2026-05-03 03:45:18 +0000

    add fingerprint from 2026 ProMaster 2500, ACC-only

--- opendbc/car/fca_giorgio/fingerprints.py
+++ opendbc/car/fca_giorgio/fingerprints.py
@@ -11,6 +11,7 @@ FW_VERSIONS = {
   },
   CAR.RAM_PROMASTER: {
     (Ecu.eps, 0x18da30f1, None): [
+      b'\x01\x01FI06FF00-0\x00\x0b',  # 2026 Promaster 2500 ACC-only
       b'\x01\x01FI06FD00-0\x00\x0b',  # 2025 Promaster 2500 ACC-only
       b'\x01\x01S2FI04EB00\x00\x0b',  # 2022 Promaster 3500 w/LKAS
     ],
