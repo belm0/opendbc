e33ac99ee3c1ff28915cb64d986bda314b272dce
John Belmonte <john@neggie.net>
2026-06-07 13:43:48 -0700

    fix test_fw_query_timing

--- opendbc/car/tests/test_fw_fingerprint.py
+++ opendbc/car/tests/test_fw_fingerprint.py
@@ -262,7 +262,7 @@ class TestFwFingerprintTiming(unittest.TestCase):
         print(f'get_vin {name} case, query time={self.total_time / self.N} seconds')
 
   def test_fw_query_timing(self):
-    total_ref_time = 8.2
+    total_ref_time = 8.3
     brand_ref_times = {
       'gm': 1.0,
       'body': 0.1,
